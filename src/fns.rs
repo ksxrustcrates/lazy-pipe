0ee8c8ab87c639e37cc152387c26c44ffd78b331
khai96_ <hvksmr1996@gmail.com>
2019-05-21 12:59:20 +0700

    add doc comment

--- src/fns.rs
+++ src/fns.rs
@@ -1,6 +1,22 @@
 use super::impls::PipeVal;
 use super::structs::PipeBox;
 
+/// Starts a pipeline
+///
+/// # Examples
+///
+/// ```rust
+/// use lazy_pipe::*;
+/// # let expected = ((3 + 1) / 2).to_string();
+///
+/// let received = pipe(3)
+///     .map(|x| x + 1)
+///     .map(|x| x / 2)
+///     .map(|x| x.to_string())
+///     .unwrap();
+///
+/// # assert_eq!(received, expected);
+/// ```
 pub fn pipe<'a, Value: 'a>(value: Value) -> PipeBox<'a, Value> {
     PipeBox::from_val(value)
 }
