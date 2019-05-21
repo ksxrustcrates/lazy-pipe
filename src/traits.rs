e1ee281c448145b373b2797c9794638298fc2dbb
khai96_ <hvksmr1996@gmail.com>
2019-05-21 13:06:31 +0700

    move aliases to their own trait

--- src/traits.rs
+++ src/traits.rs
@@ -1,17 +1,6 @@
 use super::structs::PipeBox;
 
-pub trait Pipe<'a>: PipeFunc<'a> + PipeVal<'a> + PipeMap<'a> + PipeUnwrap {
-    fn to<Next: 'a>(
-        self,
-        func: impl 'a + FnOnce(<Self as PipeMap>::Value) -> Next
-    ) -> self::PipeBox<'a, Next> {
-        self.map(func)
-    }
-
-    fn get(self) -> <Self as PipeUnwrap>::Value {
-        self.unwrap()
-    }
-}
+pub trait Pipe<'a>: PipeFunc<'a> + PipeVal<'a> + PipeMap<'a> + PipeUnwrap {}
 
 pub trait PipeFunc<'a> {
     type Value: 'a;
@@ -29,10 +18,21 @@ pub trait PipeMap<'a> {
     type Value: 'a;
 
     fn map<Next: 'a>(self, func: impl 'a + FnOnce(Self::Value) -> Next) -> PipeBox<'a, Next>;
+
+    fn to<Next: 'a>(
+        self,
+        func: impl 'a + FnOnce(<Self as PipeMap>::Value) -> Next
+    ) -> self::PipeBox<'a, Next> {
+        self.map(func)
+    }
 }
 
 pub trait PipeUnwrap {
     type Value;
 
     fn unwrap(self) -> Self::Value;
+
+    fn get(self) -> <Self as PipeUnwrap>::Value {
+        self.unwrap()
+    }
 }
